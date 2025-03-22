370a1551387ccb741032fcd98467fd0aed79bdde
gklmn <16239136+gklmn@users.noreply.github.com>
2025-03-22 19:38:19 +0100

    Ws7 Backup old changes

--- tests/test.js
+++ tests/test.js
@@ -1,8 +1,11 @@
+
+// Repère le warning "pas de tarif" et cache le parent
 var myDiv = Array.prototype.slice.call(document.querySelectorAll('notification'));
 
 myDiv.map(function (x) {
     if (x.parentNode.tagName === 'PRICE-LAYOUT') {
         x.parentNode.style.color = 'red';
+        x.parentNode.style.display = 'none';
     }
     x.style.color = "red";
     x.style.display = "none";
